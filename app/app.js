05338f39e0b93f91885029fc70bfb9db1dd584a4
ahsang <ahsangondal15@gmail.com>
2016-07-26 16:15:08 +0500

    validation

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular.module('hubDeveloperPortal', [
-    'ngRoute', 'ui.router', 'validation', 'validation.rule', 'ngStorage','ngCookies', 'colorpicker.module', 'naif.base64', 'ngDialog'
-  ]).config(['$validationProvider', function($validationProvider) {
+    'ngRoute', 'ui.router', 'validation', 'validation.rule', 'ngStorage', 'ngCookies', 'colorpicker.module', 'naif.base64', 'ngDialog'
+  ]).config(['$validationProvider', function ($validationProvider) {
     var defaultMsg;
     defaultMsg = {
       url: {
@@ -13,13 +13,20 @@
       required: {
         error: 'This is a required field',
         success: ''
+      },
+      email: {
+        error: 'This is an invalid email',
+        success: ''
       }
     };
     $validationProvider.setDefaultMsg(defaultMsg);
-  }]).run(function($rootScope, $location, $anchorScroll) {
-  //when the route is changed scroll to the proper element.
-    $rootScope.$on('$stateChangeSuccess', function(newRoute, oldRoute) {
-      console.log("success",$location.hash());
+    $validationProvider.setErrorHTML(function (msg, element, attrs) {
+        return '<p class="invalid">' + msg + '</p>';
+    });
+  }]).run(function ($rootScope, $location, $anchorScroll) {
+    //when the route is changed scroll to the proper element.
+    $rootScope.$on('$stateChangeSuccess', function (newRoute, oldRoute) {
+      console.log("success", $location.hash());
       // if($location.hash()){
       //   $anchorScroll();
       // }
--- app/states/internal/register/register.controller.js
+++ app/states/internal/register/register.controller.js
@@ -10,23 +10,22 @@
     });
   });
 
-  angular.module('hubDeveloperPortal').controller('registerCtrl', function ($location, $http, $scope, $rootScope, $state, constants, $sessionStorage, cookies) {
-    console.log("constants", constants.data.hubUrl);
+  angular.module('hubDeveloperPortal').controller('registerCtrl', function ($location, $http, $scope, $rootScope, $state, constants, $sessionStorage, cookies, $validation) {
+    $scope.form = {
+      submit: function (form) {
+        $validation.validate(form)
+        .success($scope.success)
+        .error($scope.error);
+      }
+    };
+    $scope.success = function (message) {
+      $scope.registerApplication();
+    };
 
-    // //Test Data to be removed
-    // $scope.registeredApp = {
-    //   iframeUrl: 'http://localhost:5430',
-    //   appAdminEmail: 'joncodo@gmail.com',
-    //   name: 'TriviaApp-' + Math.floor(Math.random() * (10000)),
-    //   title: 'TestApp',
-    //   applicationType: 'all',
-    //   tags: ['files'],
-    //   subTagLabel: 'Test Tag',
-    //   isPublic: false
-    // };
-    // //^^ Test Data to be removed
-
-    $scope.registerApplication = function(){
+    $scope.error = function (message) {
+      alert('Error ' + message);
+    };
+    $scope.registerApplication = function () {
       //Set the values the user did not set
       var base64Image = $scope.registeredApp.iconFont.base64;
       $scope.registeredApp.svgIcon = base64Image;
@@ -35,11 +34,17 @@
       $scope.registeredApp.apiVersion = 1;
       $scope.registeredApp.name = $scope.registeredApp.name.replace(' ', '-');
       $scope.registeredApp.title = $scope.registeredApp.name;
-
+      $scope.registeredApp.isPublic = ($scope.registeredApp.isPublic === "true");
       var credsString = '?id=' + cookies.get('email') + '&xsp=' + cookies.get('xsp') + '&pwd=' + cookies.get('password');
       $http.post(constants.data.hubUrl + '/createRegisteredApp' + credsString, $scope.registeredApp).then(function (createdApp) {
         console.log("Your application was created,", createdApp);
         $state.go('internal.sandbox');
+      }, function (err) {
+        $scope.registeredApp.isPublic = $scope.registeredApp.isPublic.toString();
+        console.log("error in creation", err);
+        if (err.data && err.data.invalidAttributes && err.data.invalidAttributes.name) {
+          $scope.error = err.data.invalidAttributes.name[0].message;
+        }
       });
     };
   });
