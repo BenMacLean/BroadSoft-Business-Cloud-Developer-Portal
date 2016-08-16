62924b6a662c4c1ff34d4bc670cef755171e638d
ahsang <ahsangondal15@gmail.com>
2016-08-17 01:05:11 +0500

    saving local cookie for requests to work

--- app/states/login/login.controller.js
+++ app/states/login/login.controller.js
@@ -16,7 +16,7 @@
     });
   });
 
-  angular.module('hubDeveloperPortal').controller('loginCtrl', function ($scope, $rootScope, $http, $state,$sessionStorage, cookies,constants) {
+  angular.module('hubDeveloperPortal').controller('loginCtrl', function ($scope, $rootScope, $http, $state,$sessionStorage, cookies,constants, $cookies) {
 
     // //TODO REMOVE FOR PROD
     // $scope.email = 'jon.staging@broadsoftlabs.com';
@@ -38,6 +38,7 @@
         };
         return $http.post(constants.data.hubUrl+'/v1/user/login',params).then(function (response) {
           cookies.set('hubLoginToken', response.data.hubLoginToken);
+          $cookies.put('local', response.data.hubLoginToken);
           cookies.set('xsp', url);
           cookies.set('email', $scope.email);
           cookies.set('password', $scope.password);
